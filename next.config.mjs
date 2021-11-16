a70779c421746c6cf2a7b3e698a7661aa3fa09da
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 13:44:46 -0600

    feat: update NextJS config

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextJSConfig = {
+  i18n: {
+    defaultLocale: 'en-US',
+    locales: ['en-US'],
+  },
+  reactStrictMode: true,
+};
+
+export default nextJSConfig;
