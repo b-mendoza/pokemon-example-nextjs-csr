36a64a6bf44c6bcfc97f47bb213162861e212aab
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 15:41:16 -0600

    feat: update `_document` component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,10 @@ class __Document extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-            crossOrigin="anonymous"
-          />
+          <link href="/favicon.ico" rel="icon" />
+          <meta content="Example app about CSR of NextJS" name="description" />
         </Head>
+
         <body>
           <Main />
           <NextScript />
