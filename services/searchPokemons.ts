0b09449be81cbbffc4c86a45cbdefd203f9162b6
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 15:49:13 -0600

    fix: remove `env` variable from `searchPokemons` service

--- services/searchPokemons.ts
+++ services/searchPokemons.ts
@@ -1,11 +1,7 @@
 import { SearchPokemonsAPIResponse } from 'typings/api';
 
 export const searchPokemons = async (searchQuery = '') => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/search?q=${encodeURI(
-      searchQuery,
-    )}`,
-  );
+  const response = await fetch(`/api/search?q=${encodeURI(searchQuery)}`);
 
   return (await response.json()) as Promise<SearchPokemonsAPIResponse>;
 };
