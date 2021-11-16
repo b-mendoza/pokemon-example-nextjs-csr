552d29d9cdec944b7eb1e957f116c2b1c20624c2
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 15:41:04 -0600

    feat: update `pokemon` API route

--- pages/api/pokemon.ts
+++ pages/api/pokemon.ts
@@ -1,29 +1,35 @@
-import { Pokemon } from 'models';
 import { NextApiRequest, NextApiResponse } from 'next';
-import pokemon from 'pokemon.json';
 
-type Data = Pokemon;
+import pokemonList from 'pokemon.json';
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  if (!req.query.name) {
-    res.statusCode = 400;
+import { GetPokemonAPIResponse } from 'typings/api';
+import { Pokemon } from 'typings/pokemon';
 
-    res.end('Must Have a Name');
-  } else {
-    const found = pokemon.filter(
-      ({ name: { english } }) => english === req.query.name,
-    );
+type Data = GetPokemonAPIResponse;
 
-    if (found.length === 0) {
-      res.statusCode = 404;
+export default function getPokemon(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (!req.query.name || typeof req.query.name !== 'string') {
+    res.status(400).json({ message: '`name` param was not provided' });
 
-      res.end(`Pokemon ${req.query.name as string} - Not Found`);
-    } else {
-      res.statusCode = 200;
-
-      res.setHeader('Content-Type', 'application/json');
-
-      res.end(JSON.stringify(found[0]));
-    }
+    return;
   }
-};
+
+  const typedPokemonList = pokemonList as Pokemon[];
+
+  const pokemon = typedPokemonList.find((pokemon) => {
+    const { name } = pokemon;
+
+    return name.english === req.query.name;
+  });
+
+  res
+    .status(pokemon ? 200 : 404)
+    .json(
+      pokemon
+        ? { pokemon }
+        : { message: `Pokemon ${req.query.name} - Not Found` },
+    );
+}
